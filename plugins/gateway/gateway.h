e166115ce5ae352ae91ec4cebf0825b8d031eabb
Ole Troan <ot@cisco.com>
2022-10-03 11:28:11 +0200

    copyright update
    Signed-off-by: Ole Troan <ot@cisco.com>

--- plugins/gateway/gateway.h
+++ plugins/gateway/gateway.h
@@ -1,19 +1,7 @@
-/*
- * Copyright (c) 2015 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#ifndef __included_gateway_h__
-#define __included_gateway_h__
+// Copyright(c) 2022 Cisco Systems, Inc.
+
+#ifndef included_gateway_h
+#define included_gateway_h
 
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
@@ -111,4 +99,4 @@ gw_set_geneve_output(gw_set_geneve_output_args_t *args);
 
 #define VCDP_GW_PLUGIN_BUILD_VER "1.0"
 
-#endif /* __included_gateway_h__ */
+#endif /* included_gateway_h */
