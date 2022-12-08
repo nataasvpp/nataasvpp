dd360090d50cfdcd47597ff12e3d329f9d9dc7a9
Ole Troan <ot@cisco.com>
2022-12-08 12:30:44 +0100

    control-plane: add more validation of internal references
    Signed-off-by: Ole Troan <ot@cisco.com>

--- control-plane/validate.py
+++ control-plane/validate.py
@@ -17,3 +17,22 @@ schema = read_jsonfile(schemaname)
 instance = read_jsonfile(instancename)
 
 validate(instance, schema)
+
+tunnel_keys = instance['tunnels'].keys()
+tenant_keys = {int(k): k for k in instance['tenants'].keys()}
+nat_keys = instance['nats'].keys()
+
+# Validate that internal references are correct
+for k,v in instance['tunnels'].items():
+    tenant = v.get('tenant', None)
+    if tenant and tenant not in tenant_keys:
+        print(f'Tenant {tenant} is not defined')
+for k,v in instance['tenants'].items():
+    nat_instance = v.get('nat-instance', None)
+    if nat_instance and nat_instance not in nat_keys:
+        print(f'NAT instance {nat_instance} is not defined')
+for k,v in instance['interfaces'].items():
+    tenant = v.get('tenant', None)
+    if tenant and tenant not in tenant_keys:
+        print(f'Tenant {tenant} is not defined')
+
