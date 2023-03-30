8f12dd51ed082a29680888936be96d5a5e694c03
Maros Ondrejicka <mondreji@cisco.com>
2023-03-30 14:51:50 +0200

    Remove unused counter

--- plugins/vcdp/timer/node.c
+++ plugins/vcdp/timer/node.c
@@ -12,14 +12,12 @@ VLIB_NODE_FN(vcdp_timer_expire_node)
   u32 thread_index = vm->thread_index;
   vcdp_per_thread_data_t *ptd = vec_elt_at_index(vcdp->per_thread_data, thread_index);
   u32 session_index;
-  u32 count = 0;
   f64 now = vlib_time_now(vm);
   ptd->current_time = now;
   vcdp_expire_timers(&ptd->wheel, now);
   vcdp_session_index_iterate_expired(ptd, session_index)
   {
     vcdp_session_remove_or_rearm(vcdp, ptd, thread_index, session_index);
-    count += 1;
   }
 
   /* TODO: some logic so that we are not called too often */
